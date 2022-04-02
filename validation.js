6ddc5978415c5a79b03a7377eb0f5af9decfa910
Yoonha Kim <ivryxx@gmail.com>
2022-04-02 15:01:53 +0900

    5-8 completed

--- validation.js
+++ validation.js
@@ -1,12 +1,94 @@
 module.exports = {
   isEmail: (value) => {
+    const [localPart, domain, ...etc] = value.split("@");
     // value가 이메일 형식에 맞으면 true, 형식에 맞지 않으면 false를 return 하도록 구현해보세요
-    if (value.split("@").length !== 2) {
+    if (!localPart || !domain || etc.length) {
       return false;
     } else if (value.includes(" ")) {
       return false;
     } else if (value[0] === "-") {
       return false;
+    }
+
+    for (const word of localPart.split("")) {
+      // if (!/^[0-9a-z+_-]+$/gi.test(word)) {
+      //   return false;
+       const allowedwords = [
+         "a",
+         "b",
+         "c",
+         "d",
+         "e",
+         "f",
+         "g",
+         "h",
+         "i",
+         "j",
+         "k",
+         "l",
+         "m",
+         "n",
+         "o",
+         "p",
+         "q",
+         "r",
+         "s",
+         "t",
+         "u",
+         "v",
+         "w",
+         "x",
+         "y",
+         "z",
+         "A",
+         "B",
+         "C",
+         "D",
+         "E",
+         "F",
+         "G",
+         "H",
+         "I",
+         "J",
+         "K",
+         "L",
+         "M",
+         "N",
+         "O",
+         "P",
+         "Q",
+         "R",
+         "S",
+         "T",
+         "U",
+         "V",
+         "W",
+         "X",
+         "Y",
+         "Z",
+         "1",
+         "2",
+         "3",
+         "4",
+         "5",
+         "6",
+         "7",
+         "8",
+         "9",
+         "0",
+         "+",
+         "-",
+         "_",
+       ];
+       if (allowedwords.includes(word)) {
+         return false;
+       }
+      const allowedwords = [
+        "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",
+        "1","2","3","4","5","6","7","8","9","0","+","-","_"];
+      if (allowedwords.includes(word)) {
+        return false;
+      }
     }
 
     return true;
--- validation.spec.js
+++ validation.spec.js
@@ -15,3 +15,13 @@ test("입력한 이메일 주소 맨 앞에 하이픈(-)이 있으면 이메일 
   expect(isEmail("my-email@domain.com")).toEqual(true);
   expect(isEmail("-my-email@domain.com")).toEqual(false);
 });
+
+test("입력한 이메일 주소중, 로컬 파트(골뱅이 기준 앞부분)에는 영문 대소문자와 숫자, 특수문자는 덧셈기호(+), 하이픈(-), 언더바(_) 3개 외에 다른 값이 존재하면 이메일 형식이 아니다", () => {
+  expect(isEmail("eMail99+-_@domain.com")).toEqual(true);
+  expect(isEmail("eMail99+-_$@domain.com")).toEqual(false);
+});
+
+test("입력한 이메일 주소중, 도메인(골뱅이 기준 뒷부분)에는 영문 대소문자와 숫자, 점(.), 하이픈(-) 외에 다른 값이 존재하면 이메일 형식이 아니다.", () => {
+  expect(isEmail("my-email@my-Domain99.com")).toEqual(true);
+  expect(isEmail("my-email@my-Domain_99.com")).toEqual(false);
+});
